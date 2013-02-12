b0f669963d69ccd5b39f5a8870e86210375aca53
Filip Ginter <filip.ginter@gmail.com>
2013-02-13 00:07:59 +0200

    Adding a simple, Tk-based client. Very much alpha-grade.

---
+++ tkclient/tkclient.py
@@ -0,0 +1,23 @@
+from Tkinter import *
+from arrow_pad import ArrowPad		
+from jabber_client import JabberClient
+
+class App:
+
+	def __init__(self,parent):
+		self.f = Frame(parent)
+		self.arrPad=ArrowPad(self.f,self.controlButtonPress)
+		self.arrPad.pack()
+		self.f.pack()
+
+		self.jabberClient=JabberClient()
+		self.jabberClient.connect()
+		
+	def controlButtonPress(self,bName):
+		self.jabberClient.sendMsg(bName)
+
+root = Tk()
+root.title('CamCtrl')
+app = App(root)
+
+root.mainloop()
